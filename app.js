5399cb57bd633f545de7b4a23627aa4ac4628b10
student <student@BA.local>
2024-01-28 21:54:24 +0200

    adding rendering engine

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const chalk = require('chalk');
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const path = require('path');
+const hbs = require('hbs');
+
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname,'public')));
+
+console.log(chalk.blueBright('Meshulami'));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res) => {
         console.log(chalk.blueBright('HomePage'));
-        res.send('HomePage');
-        res.end();
-})
+        res.render('home', {
+            title: 'HomePage'
+        });
+});
 
 app.get('/users', (req, res) => {
     console.log(chalk.blueBright('Users page'));
@@ -17,9 +27,14 @@ app.get('/users', (req, res) => {
 
 app.get('/about', (req, res) => {
     console.log(chalk.blueBright('About page'));
-    res.send('About page');
+    res.send('<p>HHHHGHHHHHH</p>');
     res.end();
 })
+
+app.use((req , res, next) =>{
+    res.render('404');
+})
+
 
 app.listen(PORT, () =>{
     console.log(chalk.bgYellowBright(`Server is running on Port ${PORT}`));
