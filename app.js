874dee2643a27b3ba8e8024738cae1232066fc4c
Shachar <info@web-college.co.il>
2024-02-04 22:09:13 +0200

    add register

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = 8080;
 const path = require('path');
 const hbs = require('hbs');
 
+const Users = [];
 
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname,'public')));
@@ -12,12 +13,35 @@ app.use(express.static(path.join(__dirname,'public')));
 console.log(chalk.blueBright('Meshulami'));
 hbs.registerPartials(__dirname + '/views/partials');
 
-app.get('/', (req, res) => {
-        console.log(chalk.blueBright('HomePage'));
-        res.render('home', {
-            title: 'HomePage'
-        });
+app.use((request, response, next) =>{
+    console.log(chalk.blueBright(`Requested URL: ${request.url}`));
+    next();
+})
+
+app.get('/', (req, response) => {
+    response.render('home', {
+        title: 'HomePage All Users',
+        users: Users
+    });
 });
+
+app.get('/register', (req, response) => {
+    response.render('register');
+});
+
+// new data sent to user
+app.post('/new-user', (request, response) => {
+    const {firstName, userName, email, password} = request.body;
+    Users.push({firstName, userName, email, password});
+    response.render('home', {
+        title: 'HomePage All Users',
+        users: Users
+    }); 
+    console.log(Users);
+});
+
+
+
 
 app.get('/users', (req, res) => {
     console.log(chalk.blueBright('Users page'));
